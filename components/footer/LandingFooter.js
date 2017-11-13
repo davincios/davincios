49dd3f8491a5dd9938f7b24d6612462355bebde9
davincios <jvf.hus@gmail.com>
2017-11-13 02:52:49 +0000

    Added components and Flymble maincode footer

---
+++ components/footer/LandingFooter.js
@@ -0,0 +1,18 @@
+
+
+
+// Import icons from assets
+import FacebookIcon from './assets/facebook-logo.svg';
+import InstagramIcon from './assets/instagram-logo.svg';
+import TwitterIcon from './assets/twitter-logo-silhouette.svg';
+import partnersImage from './assets/partnersList.png'
+import LinkedInLogo from './assets/linkedIn-logo.svg'
+// Import icons from static
+/*
+import FlymbleLogo from  'static/images/flymble-logo-white.svg'
+import paymentsImage from 'static/images/paymentsList.png'
+import KiwiLogo from  'static/images/kiwi-logo.svg'
+import IdealLogo from  'static/images/ideal-logo.svg'
+import NinjasLogo from 'static/images/ninjas-logo.svg'
+import SmallLogo from  'static/images/landing/vliegwell_trans.svg'
+*/
